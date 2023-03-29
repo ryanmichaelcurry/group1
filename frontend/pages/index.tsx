66644d1e2a109d8f6a41649cb6da76f3743b6940
will-cozart <94061650+will-cozart@users.noreply.github.com>
2023-03-29 15:54:24 -0500

    Update index.tsx
    Learning exercise

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -3,7 +3,7 @@ import { Button, Group } from "@mantine/core";
 export default function IndexPage() {
   return (
     <Group mt={50} position="center">
-      <Button size="xl">Welcome to Mantine!</Button>
+      <Button size="xl">Welcome to Legal Paper WEIGTHS!</Button>
     </Group>
   );
 }
